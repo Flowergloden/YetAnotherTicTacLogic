6830f314f88861c11d1b1c7e2c0655ec3998cad0
FlowerGolden <1327097302@qq.com>
2024-10-14 13:02:02 +0800

    refactor: replace Resources.Find with Resources.Load due to randomly exception

--- Assets/YATTL/Scripts/Config/MapConfigScriptableObject.cs
+++ Assets/YATTL/Scripts/Config/MapConfigScriptableObject.cs
@@ -4,29 +4,8 @@ public class MapConfigScriptableObject : ScriptableObject
 {
     private static MapConfigScriptableObject _instance;
 
-    public static MapConfigScriptableObject Instance
-    {
-        get
-        {
-            if (_instance is not null)
-            {
-                return _instance;
-            }
-
-            var assets = Resources.FindObjectsOfTypeAll<MapConfigScriptableObject>();
-            switch (assets.Length)
-            {
-                case > 1:
-                    Debug.LogError("Duplicate MapConfig found");
-                    break;
-                case 0:
-                    Debug.LogError("No MapConfig found");
-                    break;
-            }
-
-            return assets[0];
-        }
-    }
+    public static MapConfigScriptableObject Instance =>
+        _instance ??= Resources.Load<MapConfigScriptableObject>("Config/棋盘数据");
 
     public int mapSize = 3;
 }
