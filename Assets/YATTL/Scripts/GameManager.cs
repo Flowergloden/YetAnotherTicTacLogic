a5ed6438c569af0ebc4f642a5909f18f09e34dd8
FlowerGolden <1327097302@qq.com>
2024-10-14 13:38:45 +0800

    feat: impl game manager

---
+++ Assets/YATTL/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+public class GameManager : MonoBehaviour
+{
+    public GameManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+
+        LogicLayerUpdater.Instance.Initialize();
+        gameObject.AddComponent<InterfaceLayer>();
+    }
+}
--- Assets/YATTL/Scripts/Model/LogicLayer.cs
+++ Assets/YATTL/Scripts/Model/LogicLayer.cs
@@ -21,5 +21,5 @@ public class LogicLayer
 
 public class MapData
 {
-    public MapElementType Type = MapElementType.None;
+    public const MapElementType Type = MapElementType.None;
 }
